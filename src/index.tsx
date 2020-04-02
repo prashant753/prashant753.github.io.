168515ec11923f67801b6dc48bd424130f187af8
prashant <prashant@nineleaps.com>
2020-04-03 00:43:20 +0530

    Added plugin to extract css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
 
-import './styles/styles.css';
+import './styles/styles.scss';
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/modules/login/Login.tsx
+++ src/modules/login/Login.tsx
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 
-import './Login.css';
+import './Login.scss';
 
 export class Login extends Component<any, any> {
 
