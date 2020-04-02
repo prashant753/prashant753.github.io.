0ee5f52686e150e21fe4db00dec767c9d6d2dffe
prashant <prashant@nineleaps.com>
2020-04-02 09:28:29 +0530

    Added initial setup for webpack

---
+++ webpack.config.dev.ts
@@ -0,0 +1,54 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve('./build'),
+        filename: 'js/[name].js',
+        chunkFilename: 'js/[name].js'
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+
+    module: {
+        rules: [
+            // we use babel-loader to load our jsx and tsx files
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+
+            // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            }
+        })
+    ]
+};
